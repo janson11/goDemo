22d1a577b62946f19d52cd70eea1a67668ef771c
shanjian <shanjian@codemao.com>
2022-02-07 14:31:30 +0800

    slice

--- chapter4/main.go
+++ chapter4/main.go
@@ -24,4 +24,25 @@ func main() {
 		fmt.Printf("数组对应值：%s\n", v)
 	}
 
+	array = [5]string{"a", "b", "c", "d", "e"}
+	slice := array[2:5]
+	fmt.Println(slice)
+
+	//slice1 := make([]string, 4, 8)
+	slice1 := []string{"a", "b", "c", "d", "e"}
+	fmt.Println(len(slice1), cap(slice1))
+
+	slice2 := append(slice1, "f")
+	fmt.Println(slice2)
+	slice2 = append(slice1, "f", "g")
+	fmt.Println(slice2)
+	slice2 = append(slice1, slice...)
+	fmt.Println(slice2)
+
+	fmt.Println("+++++++++")
+	nameAgeMap := make(map[string]int)
+	nameAgeMap["xue"] = 20
+	age := nameAgeMap["xue"]
+	fmt.Println(age)
+
 }
